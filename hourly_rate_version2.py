88645beae44d987a5abe452b08d878f82debf03c
aschmidle <60407461+aschmidle@users.noreply.github.com>
2025-05-23 22:56:24 -0400

    Add files via upload

---
+++ hourly_rate_version2.py
@@ -0,0 +1,13 @@
+hrs = float(input("Enter Hours: "))
+rate = float(input("Enter Rate: "))
+
+
+if hrs > 40:
+    # print("Overtime")
+    reg_pay = hrs*rate
+    ot = (hrs - 40) * (rate * .5)
+    print(reg_pay + ot)
+else:
+    print("Regular Pay")
+    reg_pay = hrs*rate
+    print(reg_pay)
